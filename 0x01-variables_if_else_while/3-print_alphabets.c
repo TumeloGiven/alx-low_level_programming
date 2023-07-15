2342f79932486d57ce37fdd8be287067c4dc3e32
TumeloGiven <tumelogiven034@gmail.com>
2023-07-14 18:45:03 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	char lowc;
+
+	for (lowc = 'a'; lowc <= 'z'; lowc++)
+	putchar(lowc);
+
+	for (lowc = 'A'; lowc <= 'Z'; lowc++)
+	putchar(lowc)
+	putchar(lowc);
+	return (0);
+}
