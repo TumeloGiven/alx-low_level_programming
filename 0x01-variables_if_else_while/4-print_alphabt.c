91566cdb630b2928bfb6334407c1bb302284581c
TumeloGiven <tumelogiven034@gmail.com>
2023-07-14 18:53:28 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	char lowc, e, q;
+
+	e = 'e';
+	q = 'q';
+
+	for (lowc = 'a'; lowc <= 'z'; lowc++)
+	{
+		if(lowc != e && lowc != q)
+		putchar(lowc);
+	}
+	putchar('\n');
+
+	return (0);
+}
