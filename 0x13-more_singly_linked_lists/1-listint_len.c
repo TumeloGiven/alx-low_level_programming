051cae8ffe8f3c6aedef10cf969325e6a9978e55
TumeloGiven <tumelogiven034@gmail.com>
2023-08-29 14:20:56 -0700

    Task 1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -3,13 +3,13 @@
 /**
  * listint_len - Returns the number of elements in a listint_t linked list.
  * @h: A pointer to the head of the list.
- * 
+ *
  * Return: The number of elements in the list.
  */
 size_t listint_len(const listint_t *h)
 {
 	size_t count = 0;
-	
+
 	while (h != NULL)
 	{
 		c++;
